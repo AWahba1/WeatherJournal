8353e29ca69ca298194e606950e6419f8dcda9da
Ahmed Wahba <ahmedheshamwahba@hotmail.com>
2022-05-15 21:09:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express=require('express');
 const app=express();
 
 
-//undertand cors and body parser
+
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }));
